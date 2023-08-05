630296bc4f3e4235da68b2df093f017d5b9451a6
Yaman Kalaji <syk.yaman@gmail.com>
2023-08-05 19:25:12 +0100

    Remove humidity sensor and start from the beginning again

--- Experiments/Experiment4/AllSensorsTogether/AllSensorsTogether.ino
+++ Experiments/Experiment4/AllSensorsTogether/AllSensorsTogether.ino
@@ -1,7 +1,5 @@
 #include "Wire.h"
-#define SEN0562_LightSensorAddress 0x23                 //I2C Address 0x23
-#define SHT31_TempHumiditySensorAddress 0x44
-
+#define address 0x23                 //I2C Address 0x23
 void setup()
 {
   Serial.begin(9600);
@@ -9,17 +7,10 @@ void setup()
 }
 uint8_t buf[4] = {0};
 uint16_t data, data1;
-
 float Lux;
-float Humidity;
-float Temperature;
-
 void loop()
 {
-
-  writeCommand(SHT31_SOFTRESET);
-  
-  readReg(SEN0562_LightSensorAddress, 0x10, buf, 2);              //Register Address 0x10
+  readReg(0x10, buf, 2);              //Register Address 0x10
   data = buf[0] << 8 | buf[1];
   Lux = (((float)data )/1.2);
   Serial.print("LUX:");
@@ -28,15 +19,14 @@ void loop()
   Serial.print("\n");
   delay(500);
 }
-uint8_t readReg(uint8_t address, uint8_t reg, const void* pBuf, size_t size)
+uint8_t readReg(uint8_t reg, const void* pBuf, size_t size)
 {
   if (pBuf == NULL) {
     Serial.println("pBuf ERROR!! : null pointer");
   }
   uint8_t * _pBuf = (uint8_t *)pBuf;
   Wire.beginTransmission(address);
-  byte arr[] = {0x10};
-  Wire.write(arr, 1);
+  Wire.write(&reg, 1);
   if ( Wire.endTransmission() != 0) {
     return 0;
   }
