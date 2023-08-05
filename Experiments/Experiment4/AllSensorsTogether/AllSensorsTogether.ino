c0793f76f44eeb283760d8a4135f65bea0913e67
Yaman Kalaji <syk.yaman@gmail.com>
2023-08-05 20:50:19 +0100

    Temperature sensor is working now via I2C

--- Experiments/Experiment4/AllSensorsTogether/AllSensorsTogether.ino
+++ Experiments/Experiment4/AllSensorsTogether/AllSensorsTogether.ino
@@ -1,25 +1,79 @@
 #include "Wire.h"
-#define address 0x23                 //I2C Address 0x23
+               //I2C Address 0x23
+uint8_t SHT31_TempHumiditySensorAddress = 0x44;
+uint8_t SEN0562_LightSensorAddress = 0x23;
+
+//SHT31 commands
+#define SHT31_MEAS_HIGHREP_0 0x24 //MSB
+#define SHT31_MEAS_HIGHREP_1 0x00 //LSB
+
 void setup()
 {
   Serial.begin(9600);
   Wire.begin();
 }
+//Light
 uint8_t buf[4] = {0};
 uint16_t data, data1;
 float Lux;
+
+//Humidity
+
+
 void loop()
 {
-  readReg(0x10, buf, 2);              //Register Address 0x10
-  data = buf[0] << 8 | buf[1];
-  Lux = (((float)data )/1.2);
-  Serial.print("LUX:");
-  Serial.print(Lux);
-  Serial.print("lx");
+  uint8_t readbuffer[6];
+  
+  uint8_t command = SHT31_MEAS_HIGHREP_0;
+  
+  //uint8_t cmd[2];
+  //cmd[0] = command >> 8;
+  //cmd[1] = command & 0xFF;
+  Wire.beginTransmission(SHT31_TempHumiditySensorAddress);
+  Wire.write(&command, 1);
+  uint8_t command2 = SHT31_MEAS_HIGHREP_1;
+  Wire.write(&command2, 1);
+  
+  if ( Wire.endTransmission() != 0) {
+    return 0;
+  }
+  delay(20);
+  Wire.requestFrom(SHT31_TempHumiditySensorAddress, sizeof(readbuffer));
+  for (uint16_t i = 0; i < sizeof(readbuffer); i++) {
+    readbuffer[i] = Wire.read();
+  }
+  
+  int32_t stemp = (int32_t)(((uint32_t)readbuffer[0] << 8) | readbuffer[1]); //readbuffer[2] is CRC, ignored for now
+  // simplified (65536 instead of 65535) integer version of:
+  // temp = (stemp * 175.0f) / 65535.0f - 45.0f;
+  stemp = ((4375 * stemp) >> 14) - 4500;
+  float temp = (float)stemp / 100.0f;
+
+  uint32_t shum = ((uint32_t)readbuffer[3] << 8) | readbuffer[4]; //readbuffer[5] is CRC, ignored for now
+  // simplified (65536 instead of 65535) integer version of:
+  // humidity = (shum * 100.0f) / 65535.0f;
+  shum = (625 * shum) >> 12;
+  float humidity = (float)shum / 100.0f;
+
+  Serial.print("temp:");
+  Serial.print(temp);
   Serial.print("\n");
+  Serial.print("humidity:");
+  Serial.print(humidity);
+  Serial.print("\n");
+
+  ////////////////
+  //readReg(SEN0562_LightSensorAddress, 0x10, buf, 2);              //Register Address 0x10
+  //data = buf[0] << 8 | buf[1];
+  //Lux = (((float)data )/1.2);
+  //Serial.print("LUX:");
+  //Serial.print(Lux);
+  //Serial.print("lx");
+  //Serial.print("\n");
   delay(500);
 }
-uint8_t readReg(uint8_t reg, const void* pBuf, size_t size)
+
+uint8_t readReg(uint8_t address, uint8_t reg, const void* pBuf, size_t size)
 {
   if (pBuf == NULL) {
     Serial.println("pBuf ERROR!! : null pointer");
