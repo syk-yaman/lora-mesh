be0ce46dc185454ae4676a12e8588f68ec37ed0b
Yaman Kalaji <syk.yaman@gmail.com>
2023-08-19 17:40:31 +0100

    Merge the light sensor code

--- Code/Lopy4/main.py
+++ Code/Lopy4/main.py
@@ -9,10 +9,28 @@ from _pymesh_config import PymeshConfig
 from _pymesh import Pymesh
 import config
 
+from machine import I2C
+
 def sub_cb(topic, msg):
    print(msg)
 
+def send_mqtt_message():
+    while not wlan.isconnected():  
+        machine.idle()
+    print("Connected to WiFi\n")
+
+    client = MQTTClient(config.MQTT_ID, config.MQTT_BROKER,user= config.MQTT_USER, password= config.MQTT_PASSWORD, port= config.MQTT_PORT, keepalive=30, ssl=False, ssl_params={})
+
+    client.set_callback(sub_cb)
+
+    time.sleep(5)
+    client.connect()
+    client.subscribe(topic="yaman/feeds/test")
+    print("Recieved from mesh")
+    client.publish(topic="yaman/feeds/test", msg="Recieved")
+
 def new_message_cb(rcv_ip, rcv_port, rcv_data):
+    send_mqtt_message()
     ''' callback triggered when a new packet arrived '''
     print('Incoming %d bytes from %s (port %d):' %
             (len(rcv_data), rcv_ip, rcv_port))
@@ -25,6 +43,16 @@ def new_message_cb(rcv_ip, rcv_port, rcv_data):
         pycom.rgbled(0)
         time.sleep(.1)
     return
+
+def ReadLightSensor():
+    i2c = I2C(0)
+    i2c = I2C(0, I2C.MASTER) 
+    i2c.init(I2C.MASTER, baudrate=20000)
+    lightI2C = i2c.readfrom_mem(0x23, 0x10, 2)
+    lightConverted = lightI2C[0] << 8 | lightI2C[1]
+    lightValue = lightConverted / 1.2
+    i2c.deinit() 
+    return lightValue
 
 
 pycom.heartbeat(False)
